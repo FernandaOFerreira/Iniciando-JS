bdbe4faf1824f23e37d6e090a5e127c364f507d8
FernandaOFerreira <106156142+FernandaOFerreira@users.noreply.github.com>
2023-09-07 09:45:58 -0300

    Create Day1.js
    Operadores de comparação == e ===

---
+++ Day1.js
@@ -0,0 +1,26 @@
+let numero1 = 1
+let string1 = '1'
+let numero10 = 10
+let string10 = '10'
+let numero30 = 30
+let string30 = '30'
+
+if (numero1 == string1){
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')  
+} else{
+   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (numero10 == string1) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+
+if (numero30 === string30) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+
